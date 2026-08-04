4e7865004a041f1d461cf50222c21e1bc98244bd
jlacis <j.lacis@mdeg-digital.com>
2026-08-04 15:30:15 +0200

    feat(renderer): Add PCM capture Web Audio worklet

---
+++ src/renderer/assets/nexy-pcm-capture.worklet.js
@@ -0,0 +1,10 @@
+/* global AudioWorkletProcessor, registerProcessor */
+class NexyPcmCaptureProcessor extends AudioWorkletProcessor {
+  process(inputs) {
+    const channel = inputs[0] && inputs[0][0]
+    if (channel && channel.length) this.port.postMessage(channel.slice())
+    return true
+  }
+}
+
+registerProcessor('nexy-pcm-capture', NexyPcmCaptureProcessor)
